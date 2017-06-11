2ffed46ea22b221a9c0441a44f0ce85d638c3a60
Patrick <pmoore4181@gmail.com>
2017-06-10 21:52:27 -0500

    got it working. wahoo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyAwy5j_zxQbdFMoNi_38k6FOD0jk5XLmmg",
-	authDomain: "train-schedule-8c8a6.firebaseapp.com",
-	databaseURL: "https://train-schedule-8c8a6.firebaseio.com",
-	projectId: "train-schedule-8c8a6",
-	storageBucket: "train-schedule-8c8a6.appspot.com",
-	messagingSenderId: "716943721472"
+    apiKey: "AIzaSyAwy5j_zxQbdFMoNi_38k6FOD0jk5XLmmg",
+    authDomain: "train-schedule-8c8a6.firebaseapp.com",
+    databaseURL: "https://train-schedule-8c8a6.firebaseio.com",
+    projectId: "train-schedule-8c8a6",
+    storageBucket: "train-schedule-8c8a6.appspot.com",
+    messagingSenderId: "716943721472"
 };
 firebase.initializeApp(config);
+
+// Current Time
+var currentTime = moment();
+var currentTimeNew = moment(currentTime).format("hh:mm");
+console.log(currentTimeNew);
 
 // variable to reference firebase
 var database = firebase.database();
@@ -28,6 +33,36 @@ $("#addTrain").on("click", function(event) {
     trainDestination = $("#addTrainDestination").val().trim();
     trainFirstTime = $("#firstTrainTime").val().trim();
     trainFrequency = $("#addTrainFrequency").val().trim();
+    trainFirstTime = $("#firstTrainTime").val().trim();
+
+    var newFormat = "HH:mm";
+    var trainFirstTimeNew = moment(trainFirstTime, "HH:mm");
+    console.log(trainFirstTimeNew);
+
+    console.log(trainFrequency);
+
+
+
+
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(trainFirstTimeNew), "minutes");
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainFrequency;
+    var tRemainderNew = moment(tRemainder, "HH:mm");
+    console.log(tRemainderNew);
+
+    // Minute Until Train
+    var tMinutesTillTrain = trainFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrainNew = moment(nextTrain).format("hh:mm")
+        // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
     tableRow = $("<tr>");
 
     // create new table row on click
@@ -37,41 +72,32 @@ $("#addTrain").on("click", function(event) {
 
     // append info to each section
     $(tableRow).append("<td id='trainName-" + counter + "'>");
-    	$("#trainName-" + counter).text(trainName);
+    $("#trainName-" + counter).text(trainName);
     $(tableRow).append("<td id='trainDestination-" + counter + "'>");
-    	$("#trainDestination-" + counter).text(trainDestination);
+    $("#trainDestination-" + counter).text(trainDestination);
     $(tableRow).append("<td id='trainFrequency-" + counter + "'>");
-    	$("#trainFrequency-" + counter).text(trainFrequency);
+    $("#trainFrequency-" + counter).text(trainFrequency);
     $(tableRow).append("<td id='nextArrival-" + counter + "'>");
-    	
+    $("#nextArrival-" + counter).text(nextTrainNew);
     $(tableRow).append("<td id='minutesAway-" + counter + "'>");
+    $("#minutesAway-" + counter).text(tMinutesTillTrain);
 
     // set firebase info
-    database.ref().set({
-      trainName: trainName,
-      trainDestination: trainDestination,
-      trainFrequency: trainFrequency,
-      trainFirstTime: trainFirstTime
+    database.ref().push({
+        trainName: trainName,
+        trainDestination: trainDestination,
+        trainFrequency: trainFrequency,
+        trainFirstTime: trainFirstTime,
+        currentTime: currentTimeNew
     });
 
     counter++;
 
+    trainName = "";
+    trainDestination = "";
+    trainFirstTime = "";
+    trainFrequency = "";
+
+
+
 });
-
-// Create Firebase "watcher" Hint: .on("value")
-// Firebase watcher + initial loader HINT: .on("value")
-database.ref().on("value", function(snapshot) {
-
-  // Log everything that's coming out of snapshot
-  console.log(snapshot.val());
-  console.log(snapshot.val().trainName);
-  // console.log(snapshot.val().email);
-  // console.log(snapshot.val().age);
-  // console.log(snapshot.val().comment);
-
-
-  // Handle the errors
-}, function(errorObject) {
-  console.log("Errors handled: " + errorObject.code);
-});
-
